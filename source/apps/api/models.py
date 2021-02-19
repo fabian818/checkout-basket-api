34f4fcecbcd4e1a3b2fcb2257921c446b9162a06
Fabian Pierre Peña Jacobo <f.pena.jacobo@gmail.com>
2021-02-19 18:28:43 -0500

    ADD: models configuration

--- source/apps/api/models.py
+++ source/apps/api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class DateTable(models.Model):
+    '''
+    MetaModel to save timestamp fields configuration
+    '''
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Basket(DateTable):
+    total = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+
+
+class Product(DateTable):
+    code = models.CharField(max_length=15)
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    '''
+    code field should be indexed because of potential filter querys using this field
+    '''
+    indexes = [
+       models.Index(fields=['code']),
+    ]
+
+
+class Association(DateTable):
+    basket = models.foreignkey(Basket, on_delete=models.cascade, null=False)
+    product = models.foreignkey(Product, on_delete=models.cascade, null=False)
+    quantity = models.IntegerField(null=False, default=1)
