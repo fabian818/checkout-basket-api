1628dc442acee3e70c4b8e206960827f2532fc28
Fabian Pierre Peña Jacobo <f.pena.jacobo@gmail.com>
2021-02-22 00:15:08 -0500

    FIX: lint errors

--- source/checkout_basket_api/urls.py
+++ source/checkout_basket_api/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include, re_path 
-
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
+SchemaView = get_schema_view(
    openapi.Info(
       title="Checkout Basket API",
       default_version='v1',
@@ -17,6 +16,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^redoc/$', SchemaView.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('apps.api.urls')),
 ]
